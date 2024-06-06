475dd12bda99a9c9fd41eda17cb24c81bb33a726
Shrikant Pandey <shrikantpandeydotnet@gmail.com>
2024-06-06 21:27:42 +0800

    check in student.

---
+++ ITExpertsEraFeb2024Batch/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITExpertsEraFeb2024Batch
+{
+    internal class Student
+    {
+        public int StudentId { get; set; }
+    }
+}
