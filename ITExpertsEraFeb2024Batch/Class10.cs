0e1218eed41ef623076e9b64daff37b20b5f75a3
Shrikant Pandey <shrikantpandeydotnet@gmail.com>
2024-06-06 21:33:13 +0800

    check in test class

---
+++ ITExpertsEraFeb2024Batch/Class10.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITExpertsEraFeb2024Batch
+{
+    internal class Class10
+    {
+
+        public int TestProperty { get; set; }
+    }
+}
